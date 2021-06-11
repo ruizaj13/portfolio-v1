7d022088e35e28a89240191320deff0168192d2e
ruizaj13 <ruizaj13@gmail.com>
2021-06-11 16:29:16 -0700

    carousel setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from 'react';
 import { Button } from 'antd';
 import Intro from './components/Intro';
+import Projects from './components/Projects';
 import './App.css';
 
 const App: FC = () => {
   return (
     <div className="App">
       <Intro/>
+      <Projects/>
     </div>
   );
 }
---
+++ src/components/Projects.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from 'react';
+import { Carousel } from 'antd';
+import Styled from 'styled-components';
+
+const ProjectCarousel = Styled.div`
+    background-color: yellow;
+    height: 100vh;
+`
+
+// const Arrow = ({ type, style, className, onClick }) => (
+//     <Icon type={type} style={style} className{className} onClick={onClick}/>
+// )
+
+// const contentStyle = {
+//     height: '160px',
+//     color: '#fff',
+//     lineHeight: '160px',
+//     textAlign: 'center',
+//     background: '#364d79',
+//   };
+
+const Projects: FC = () => {
+    return (
+        <>
+            <Carousel autoplay arrows>
+                <div>
+                  <h3 style={{height:'100vh', color: '#fff', lineHeight: '160px', textAlign: 'center', background: '#364d79'}}>1</h3>
+                </div>
+                <div>
+                  <h3 style={{height:'100vh', color: '#fff', lineHeight: '160px', textAlign: 'center', background: '#364d79'}}>2</h3>
+                </div>
+                <div>
+                  <h3 style={{height:'100vh', color: '#fff', lineHeight: '160px', textAlign: 'center', background: '#364d79'}}>3</h3>
+                </div>
+                <div>
+                  <h3 style={{height:'100vh', color: '#fff', lineHeight: '160px', textAlign: 'center', background: '#364d79'}}>4</h3>
+                </div>
+            </Carousel>
+        </>
+    )
+}
+
+export default Projects;
